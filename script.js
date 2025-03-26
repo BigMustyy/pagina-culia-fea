2b039118e1a6564203f8d79623e762944813e8fd
BigMustyy <benjamin.cubillos213@gmail.com>
2025-03-25 21:34:39 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+// Selecciona el botón y el cuerpo
+const button = document.getElementById("changeBackgroundButton");
+const body = document.body;
+
+// Cambia el fondo cuando el botón está presionado
+button.addEventListener("mousedown", function() {
+    body.style.backgroundImage = 'url("fondo.png")'; // Imagen al presionar
+});
+
+// Vuelve al fondo original al soltar el botón
+button.addEventListener("mouseup", function() {
+    body.style.backgroundImage = 'url("fondo2.jpg")'; // Imagen original
+});
+
 document.getElementById("SoundButton").addEventListener("click", function() {
     const audio = document.getElementById("audio");
     audio.play();
 });
+
